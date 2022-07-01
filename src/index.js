a3e090784cce3220b6a06887540c62c5ae5a2252
A_M_ <104923204+lkhgvjgfxdrdhxtfc@users.noreply.github.com>
2022-07-01 19:55:56 +0300

    feat: add code in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let n1 = Math.abs(n);
+        let result ='';
+        for (let i=n1.toString().length-1; i>=0; i=i-1) {
+          result = result + n1.toString()[i];
+        }
+        return result;
 }
